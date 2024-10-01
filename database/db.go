270664e665af8072348774e1967845d3fc436ff5
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-10-01 20:38:50 -0300

    Ajuste de variáveis de ambiente e melhoria na conexão com banco de dados Postgres

--- database/db.go
+++ database/db.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
@@ -14,11 +16,29 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	// Pegando as variáveis de ambiente
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
+
+	// Construindo a string de conexão com o banco de dados
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		endereco, usuario, senha, nomeBanco, portaBanco,
+	)
+
+	// Abrindo a conexão com o banco de dados
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panicf("Erro ao conectar com banco de dados: %v", err)
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	// Migrando o modelo 'Aluno'
+	if err = DB.AutoMigrate(&models.Aluno{}); err != nil {
+		log.Panicf("Erro ao migrar o modelo: %v", err)
+	}
+
+	log.Println("Conexão com banco de dados estabelecida com sucesso")
 }
